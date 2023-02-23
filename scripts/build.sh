312f6a6756028a6b839de89553dfae2db2e256ec
Agnes Kiss <agnes@snowplowanalytics.com>
2023-02-23 12:31:26 +0000

    Modify build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,6 +19,7 @@ mkdir build/themes
 cp -R accelerator-web-ui-template/themes/hugo-theme-learn build/themes/
 cp -R accelerator-web-ui-template/layouts build/
 cp -R accelerator-web-ui-template/static build/
+cp -R accelerator-web-ui-template/data build/
 cd build
 
 echo "Creating Hugo site..."
